15a8ecefb4c92a9178c504356f749eb995a22fb9
mo <salahkoyilandy@gmail.com>
2023-08-01 10:48:31 +0530

    api key modified

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,8 +6,8 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyDfv-rpj1MaJ62bd3WcarzoJfFENuOpmtk"];
+  [GMSServices provideAPIKey:@"AIzaSyAsbzthbPkbqRK3vzxc1P8TftPRvOYH1P0"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-@end
+@end0
